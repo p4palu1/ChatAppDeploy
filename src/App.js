7b9cc6c014597ac781f543bc61ac7e5736df9f62
Reem Stamker <reem.stamker@gmail.com>
2021-09-17 17:46:49 +0300

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -1,89 +1,130 @@
-import React, { Component } from 'react';
-import Video from "./video";
-import t1 from "./t1.jpg";
-import t2 from "./t2.png";
-import Header from "./header";
-import Menu from "./menu"
-import Feed from "./feed";
-import SearchedFeed from './searchFeed'
-import "./app.css"
-import $ from "jquery";
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState, useRef } from "react"
+import logo from './logo.svg';
+import './App.css';
+//firebase SDK
+import firebase from "firebase/compat/app"
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+//firebase hooks
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useCollectionData } from "react-firebase-hooks/firestore"
+//bootstrap
+import { Button } from "react-bootstrap"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAQGA-1usi-_1_kJ0yOhzHjYs2Gp2GsGWo",
+  authDomain: "message-app-ad578.firebaseapp.com",
+  projectId: "message-app-ad578",
+  storageBucket: "message-app-ad578.appspot.com",
+  messagingSenderId: "852222317831",
+  appId: "1:852222317831:web:bd8078c6eaef430a66a1a2",
+  measurementId: "G-B5RRC17SM3"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 function App() {
-  const dates = [new Date(2021, 2, 1), new Date(2019, 7, 1), new Date(2020, 2, 11), new Date(2001, 6, 10)]
 
-  const vids = [
-    {
-      title: "How to make an eye catching thumbnail",
-      desc: "uno desc",
-      views: 100244,
-      channel: "reem",
-      thumbnail: t1, 
-      date: dates[0],
-      url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
-      id: 1
-    },
-    {
-      title: "dos",
-      desc: "dos desc",
-      views: 140244,
-      channel: "reem",
-      thumbnail: t2,
-      date: dates[1],
-      url: "https://www.youtube.com/watch?v=kK_Wqx3RnHk",
-      id: 2
-    },
-    {
-      title: "Intense Israel-Palestine debate MUST WATCH!",
-      desc: "dos desc",
-      views: 2340763347,
-      channel: "reem",
-      thumbnail: t2,
-      date: dates[2],
-      url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
-      id: 3
-    },
-    {
-      title: "Neymar JR highlight VS AC Monaco",
-      desc: "dos desc",
-      views: 3347,
-      channel: "Football",
-      thumbnail: t2,
-      date: dates[3],
-      url: "https://www.youtube.com/watch?v=kK_Wqx3RnHk",
-      id: 4
-    }
-  ]
+  const [user] = useAuthState(auth);
 
   return (
-    <div className="App" id="app">
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
 
-      <Router>
-          <Switch>
-              <Route path="/search/:searchTerm">
-                <Header />
-                <Menu  />
-                <SearchedFeed vids={vids}/>
-              </Route>
-              <Route path="/">
-                  <Header />
-                  <Menu  />
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
 
-                  <Feed vids={vids}/>
-              </Route>
-          </Switch>
-      </Router>
-      
-      
-        
-         
     </div>
   );
 }
-  
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
 
 
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
 
 
 export default App;
